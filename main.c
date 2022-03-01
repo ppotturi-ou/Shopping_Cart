12bd6fea16f346b0999174e968dff0c381859452
ppotturi-ou <ppotturi@oakland.edu>
2022-03-01 09:33:45 -0500

    Basic_Project_Template
    Shopping cart template

---
+++ main.c
@@ -0,0 +1,23 @@
+/****************************************
+Project: Shopping Cart with Built in billing system
+Members: 
+
+Course: ECE 5721/4721 Winter 2022
+Oakland University
+
+
+
+****************************************/
+
+int main(void)
+{
+
+	while(1)
+	{
+	
+	
+	}
+
+
+}
+
